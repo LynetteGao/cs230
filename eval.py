ea1fbf20d0577269dc94fee663f667d7f55cbc31
lynette <qgao38@wisc.edu>
2021-10-31 22:18:44 -0700

    first version

---
+++ eval.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+import tensorflow as tf
+
+from util import make_w2v_embeddings
+from util import split_and_zero_padding
+from util import ManDist
+
+# File paths
+
+TEST_CSV = "./tsv/test.tsv"
+
+# Load training set
+test_df = pd.read_table(TEST_CSV,header = None, names = ['id','qid1','qid2',
+                                                              
+                                                              'question1', 'question2','is_duplicate'])
+for q in ['question1', 'question2']:
+    test_df[q + '_n'] = test_df[q]
+
+# Make word2vec embeddings
+embedding_dim = 300
+max_seq_length = 20
+test_df, embeddings = make_w2v_embeddings(test_df, embedding_dim=embedding_dim, empty_w2v=False)
+
+# Split to dicts and append zero padding.
+X_test = split_and_zero_padding(test_df, max_seq_length)
+
+# Make sure everything is ok
+assert X_test['left'].shape == X_test['right'].shape
+
+
