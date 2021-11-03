1ce5994d7d406ab593e9cfe622f499ce995e61e3
lynette <qgao38@wisc.edu>
2021-11-02 17:30:46 -0700

    s

--- eval.py
+++ eval.py
@@ -10,8 +10,26 @@ from util import ManDist
 M = './SiameseLSTM2.h5'
 TEST_CSV = "./tsv/test.tsv"
 
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+    
 model = tf.keras.models.load_model(M, custom_objects={'ManDist': ManDist})
 model.summary()
+
 
 
 # Load training set
